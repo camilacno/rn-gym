81019dfdd44189471bbe165b0c1a904fa2667c3c
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-08 16:13:29 -0300

    Add svg config + SignIn screen WIP

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
-import { VStack, Image } from 'native-base'
+import { VStack, Image, Text, Center, Heading } from 'native-base'
+
+import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 
 export function SignIn() {
@@ -10,6 +12,20 @@ export function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e seu corpo
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+      </Center>
     </VStack>
   )
 }
