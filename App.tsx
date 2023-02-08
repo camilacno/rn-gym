b231283482f17645aaaf714be8c954ce94f337e7
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-07 22:32:34 -0300

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { Home } from '@screens/Home'
+import { Loading } from '@components/Loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {!fontsLoaded ? <Loading /> : <Home />}
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export function Loading() {
+  return (
+    <View>
+      <Text>Loading</Text>
+    </View>
+  )
+}
