83873343dc64a3042d164d5251faf7564ef88315
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-08 18:15:42 -0300

    Navigation dependencies

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, View } from 'react-native'
+import { StatusBar } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
 import {
   useFonts,
@@ -10,7 +10,6 @@ import { THEME } from './src/theme'
 
 import { Loading } from '@components/Loading'
 import { SignIn } from '@screens/SignIn'
-import { SignUp } from '@screens/SignUp'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -22,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   )
 }
