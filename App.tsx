8de62364e870be762bc351e8ebeac6f33b62656a
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-08 15:56:04 -0300

    SignIn structure

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import {
 import { THEME } from './src/theme'
 
 import { Loading } from '@components/Loading'
+import { SignIn } from '@screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -20,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   )
 }
--- src/screens/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
