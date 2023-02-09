300215ebde715141b8076cf4e81a77cbdf0fc04a
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-09 13:23:49 -0300

    Create tab navigation screens

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import { THEME } from './src/theme'
 
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
+import { Home } from '@screens/Home'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -21,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   )
 }
