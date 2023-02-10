e39b92d1cd70e73b2a051ed6b6e961b999a01d14
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-09 21:27:35 -0300

    Group component and home flatlist (groups)

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,20 +1,36 @@
-import { TouchableOpacity } from 'react-native'
+import { Pressable, IPressableProps } from 'native-base'
 import { Text } from 'native-base'
 
-type GroupProps = {
+type GroupProps = IPressableProps & {
   name: string
+  isActive: boolean
 }
-export function Group({ name }: GroupProps) {
+export function Group({ name, isActive, ...rest }: GroupProps) {
   return (
-    <TouchableOpacity>
+    <Pressable
+      {...rest}
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: 'green.500',
+        borderWidth: 1,
+      }}
+    >
       <Text
-        color="gray.200"
+        color={isActive ? 'green.500' : 'gray.200'}
         textTransform="uppercase"
-        fontSize="xs"
+        fontSize="sm"
         fontWeight="semibold"
       >
         {name}
       </Text>
-    </TouchableOpacity>
+    </Pressable>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,38 @@
+import { useState } from 'react'
+import { FlatList, VStack } from 'native-base'
+
 import { Group } from '@components/Group'
 import { HeaderHome } from '@components/HeaderHome'
-import { Text, VStack } from 'native-base'
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    'costas',
+    'biceps',
+    'triceps',
+    'ombros',
+  ])
+  const [groupSelected, setGroupSelected] = useState('')
+
   return (
     <VStack flex={1}>
       <HeaderHome />
-      <Group name="costas" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            onPress={() => setGroupSelected(item)}
+            isActive={groupSelected === item}
+          />
+        )}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
