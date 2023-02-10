5d8ed0c8f6efb106a3235954d3805783b1b954f3
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-10 16:50:17 -0300

    History card component

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,38 @@
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Heading, HStack, Text, VStack } from 'native-base'
+
+type HistoryCardProps = TouchableOpacityProps & {}
+
+export function HistoryCard({ ...rest }: HistoryCardProps) {
+  return (
+    <HStack
+      w-full
+      px={5}
+      py={4}
+      mb={3}
+      bg="gray.600"
+      rounded="md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr={5}>
+        <Heading
+          fontSize="md"
+          color="white"
+          fontFamily="heading"
+          textTransform="capitalize"
+        >
+          Costas
+        </Heading>
+
+        <Text fontSize="lg" color="gray.100" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
+
+      <Text color="gray.300" fontSize="md">
+        08:32
+      </Text>
+    </HStack>
+  )
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,15 @@
 import { VStack } from 'native-base'
 import { HeaderScreens } from '@components/index'
+import { HistoryCard } from '@components/HistoryCard'
 
 export function History() {
   return (
     <VStack flex={1}>
       <HeaderScreens title="Histórico de exercícios" />
+
+      <VStack mx={5} mt={10}>
+        <HistoryCard />
+      </VStack>
     </VStack>
   )
 }
