fd5a7514e7df1f0bf009e89c24b41595c1dcff86
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-10 17:27:15 -0300

    Profile user image

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,20 @@
-import { VStack } from 'native-base'
-import { HeaderScreens } from '@components/index'
+import { Center, ScrollView, VStack } from 'native-base'
+import { HeaderScreens, UserPhoto } from '@components/index'
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <HeaderScreens title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{ uri: 'https://github.com/camilacno.png' }}
+            alt="Imagem de perfil"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
