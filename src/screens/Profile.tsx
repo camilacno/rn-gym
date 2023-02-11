3d4138b9b4404c8660f2aa67f23e4aa85ee316ee
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-10 22:15:51 -0300

    Profile photo skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,33 @@
-import { Center, ScrollView, VStack } from 'native-base'
+import { useState } from 'react'
+import { Center, ScrollView, VStack, Skeleton } from 'native-base'
 import { HeaderScreens, UserPhoto } from '@components/index'
 
+const PHOTO_SIZE = 33
+
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <HeaderScreens title="Perfil" />
 
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: 'https://github.com/camilacno.png' }}
-            alt="Imagem de perfil"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/camilacno.png' }}
+              alt="Imagem de perfil"
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
