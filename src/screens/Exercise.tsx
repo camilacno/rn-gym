9bac0829d4d2ba7068666feddf1b4c198f9c9768
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-11 13:01:34 -0300

    Exercise screen image

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,11 @@
 import { TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
-import { Center, Text, VStack, Icon } from 'native-base'
+import { Text, VStack, Icon, HStack, Heading, Image } from 'native-base'
 import { Feather } from '@expo/vector-icons'
 
 import { AppNavigatiorRoutesProps } from '@routes/app.routes'
+
+import BodyIcon from '@assets/body.svg'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatiorRoutesProps>()
@@ -17,6 +19,37 @@ export function Exercise() {
         <TouchableOpacity onPress={handleGoBack}>
           <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
         </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          mt={4}
+          mb={8}
+          alignItems="center"
+        >
+          <Heading color="gray.100" fontSize="lg" flexShrink={1} pr={4}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodyIcon />
+            <Text color="gray.100" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{
+            uri: 'https://studiomedinaplus.com.br/wp-content/uploads/2020/04/imagem-remada-unilateral-2138125.jpg',
+          }}
+          alt="Imagem exercício"
+          rounded="lg"
+          resizeMode="cover"
+        />
       </VStack>
     </VStack>
   )
