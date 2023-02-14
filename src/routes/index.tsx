f2811f62be54740a922abe3bddeebab257232cc0
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-14 14:16:35 -0300

    Auth hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,18 @@
-import { useContext } from 'react'
 import { useTheme, Box } from 'native-base'
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 
-import { AuthContext } from '@contexts/AuthContext'
+import { useAuth } from '@hooks/useAuth'
 import { AuthRoutes } from './auth.routes'
 import { AppRoutes } from './app.routes'
 
 export function Routes() {
   const { colors } = useTheme()
-
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
 
-  const contextData = useContext(AuthContext)
-  console.log('Usuário logado', contextData)
+  const { user } = useAuth()
 
+  console.log('Usuário logado', user)
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
