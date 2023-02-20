a39cadc22a74b2710b0941c5669cdaada4cc0316
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-19 22:19:22 -0300

    Update and save updated token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosInstance, AxiosError } from 'axios'
 import { AppError } from '@utils/AppError'
-import { storageAuthTokenGet } from '@storage/storageAuthToken'
+import {
+  storageAuthTokenGet,
+  storageAuthTokenSave,
+} from '@storage/storageAuthToken'
 
 type SignOut = () => void
 
@@ -55,6 +58,27 @@ api.registerInterceptTokenManager = (signOut) => {
           }
 
           isRefreshing = true
+
+          return new Promise(async (resolve, reject) => {
+            try {
+              const { data } = await api.post('/sessions/refresh-token', {
+                refresh_token,
+              })
+              storageAuthTokenSave({
+                token: data.token,
+                refresh_token: data.refresh_token,
+              })
+            } catch (error: any) {
+              failedQueue.forEach((request) => {
+                request.onFailure(error)
+              })
+              signOut()
+              reject(error)
+            } finally {
+              isRefreshing = false
+              failedQueue = []
+            }
+          })
         }
 
         signOut()
