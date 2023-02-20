f53e5974ced45ac324938a0c9fd4e83415ba5254
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-19 22:10:37 -0300

    Refresh token add requests to queue

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosInstance, AxiosError } from 'axios'
 import { AppError } from '@utils/AppError'
+import { storageAuthTokenGet } from '@storage/storageAuthToken'
 
 type SignOut = () => void
+
+type PromiseType = {
+  onSuccess: (token: string) => void
+  onFailure: (error: AxiosError) => void
+}
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void
@@ -11,14 +17,53 @@ const api = axios.create({
   baseURL: 'http://192.168.18.27:3333',
 }) as APIInstanceProps
 
+let failedQueue: PromiseType[] = []
+let isRefreshing = false
+
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
-    (error) => {
-      if (error.response && error.response.data) {
-        return Promise.reject(new AppError(error.response.data.message))
+    async (requestError) => {
+      if (requestError?.response?.status === 401) {
+        if (
+          requestError.response.data?.message === 'token.expired' ||
+          requestError.response.data?.message === 'token.invalid'
+        ) {
+          const { refresh_token } = await storageAuthTokenGet()
+
+          if (!refresh_token) {
+            signOut()
+            return Promise.reject(requestError)
+          }
+
+          const originalRequestConfig = requestError.config
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token: string) => {
+                  originalRequestConfig.headers = {
+                    Authorization: `Bearer ${token}`,
+                  }
+                  resolve(api(originalRequestConfig))
+                },
+                onFailure: (error: AxiosError) => {
+                  reject(error)
+                },
+              })
+            })
+          }
+
+          isRefreshing = true
+        }
+
+        signOut()
+      }
+
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message))
       } else {
-        return Promise.reject(error)
+        return Promise.reject(requestError)
       }
     }
   )
